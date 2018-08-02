4cf540467845f4c6116c5c9f059592cd3a4639be
jmartuccelli <30668423+jmartuccelli@users.noreply.github.com>
2018-08-02 11:25:29 -0400

    Update and rename launcher.py to alpha-launcher.py

--- alpha-launcher.py
+++ alpha-launcher.py
@@ -1,6 +1,12 @@
 import subprocess
 from subprocess import call
 call(["vim", "/etc/yum.repos.d/ugene.repo"])
+write '[ugene]
+name=ugene
+baseurl=https://raw.githubusercontent.com/jmartuccelli/ugene/master/
+enabled=1
+gpgcheck=0'
+# :x command needed save vim file
 call(['yum', 'clean all'])
 call (['yum'], 'info ugene')
 
