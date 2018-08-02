915442e872eb2503b339c7bb6efda661f2672cfd
jmartuccelli <30668423+jmartuccelli@users.noreply.github.com>
2018-08-02 11:39:31 -0400

    Update alpha-launcher.py

--- alpha-launcher.py
+++ alpha-launcher.py
@@ -1,5 +1,7 @@
 import subprocess
 from subprocess import call
+##import os to use su?
+##su+prompt password not trustworthy
 call(["vim", "/etc/yum.repos.d/ugene.repo"])
 write '[ugene]
 name=ugene
@@ -10,5 +12,10 @@ gpgcheck=0'
 call(['yum', 'clean all'])
 call (['yum'], 'info ugene')
 
+##yum install ugene
+
 #not workng yet
 #need to write text to vim file then save
+#might be more efficient if people just create the vim file,
+#it is easier, 
+#if people cant do it thats a bad sign
