1eba04a70a6607ca6dffd795bec592d6230c3164
jmartuccelli <30668423+jmartuccelli@users.noreply.github.com>
2018-08-02 13:27:44 -0400

    Update alpha-launcher.py

--- alpha-launcher.py
+++ alpha-launcher.py
@@ -1,18 +1,8 @@
 import subprocess
 from subprocess import call
-##import os to use su?
-##su+prompt password not trustworthy
-call(["vim", "/etc/yum.repos.d/ugene.repo"])
-write '[ugene]
-name=ugene
-baseurl=https://raw.githubusercontent.com/jmartuccelli/ugene/master/
-enabled=1
-gpgcheck=0'
-# :x command needed save vim file
-call(['yum', 'clean all'])
-call (['yum'], 'info ugene')
-
-f = open("ugene.repo", "name=ugene")
+#reqirement run as root user
+f = open("ugene.repo", "w")
+f.write ('name=ugene')
 f.write("baseurl=https://raw.githubusercontent.com/jmartuccelli/ugene/master/")
 f.write('enabled=1')
 f.write('gpgcheck=0')
