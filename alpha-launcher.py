e28cecccd3124e49c0a09f81a31a5d37248287ad
jmartuccelli <30668423+jmartuccelli@users.noreply.github.com>
2018-08-02 13:31:27 -0400

    Update alpha-launcher.py

--- alpha-launcher.py
+++ alpha-launcher.py
@@ -2,9 +2,9 @@ import subprocess
 from subprocess import call
 #reqirement run as root user
 f = open("ugene.repo /etc/yum.repos.d/", "w")
-f.write 9'[ugene]'
+f.write ('[ugene]')
 f.write ('name=ugene')
-f.write("baseurl=https://raw.githubusercontent.com/jmartuccelli/ugene/master/")
-f.write('enabled=1')
-f.write('gpgcheck=0')
+f.write ("baseurl=https://raw.githubusercontent.com/jmartuccelli/ugene/master/")
+f.write ('enabled=1')
+f.write ('gpgcheck=0')
 ##
